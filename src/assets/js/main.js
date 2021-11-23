6772c9fc8d694ef00585142dbe326e676e6f2a7f
Sara Soueidan <sara.soueidan@gmail.com>
2021-11-23 12:03:55 +0200

    update installation snippet copy functionality

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,13 @@
 (function() {
-const copyBtn = document.getElementById('copyBtn')
-    const copyText = document.getElementById('code-snippet')
+    const copyBtn = document.getElementById('copyBtn');
+    const copyText = document.getElementById('code-snippet');
+    const liveRegion = document.getElementById('copy-update');
     copyBtn.removeAttribute('hidden');
 
     copyBtn.onclick = () => {
         copyText.select();
         document.execCommand('copy');
+        liveRegion.innerHTML = "Installation code copied to clipboard."
         this.focus();
     }
 })();
