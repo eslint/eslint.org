99d7f0a25d304baa80d2fae948be19d6bdaaf9c7
Sara Soueidan <sara.soueidan@gmail.com>
2021-11-25 08:40:09 +0200

    add behavior to navigation on mobile

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,25 @@
 (function() {
-    const copyBtn = document.getElementById('copyBtn');
-    const copyText = document.getElementById('code-snippet');
-    const liveRegion = document.getElementById('copy-update');
-    copyBtn.removeAttribute('hidden');
+  var nav_trigger = document.getElementById("nav-toggle"),
+    nav = document.getElementById("nav-list"),
+    body = document.getElementsByTagName("body")[0],
+    open = false;
 
-    copyBtn.onclick = () => {
-        copyText.select();
-        document.execCommand('copy');
-        liveRegion.innerHTML = "Installation code copied to clipboard."
-        this.focus();
+    nav_trigger.removeAttribute("hidden");
+    nav_trigger.setAttribute("aria-expanded", "false");
+    nav.setAttribute("data-open", "false");
+
+    nav.setAttribute("data-open", "false");
+    nav_trigger.addEventListener("click", togglenav, false);
+
+  function togglenav(e) {
+    if (!open) {
+      this.setAttribute("aria-expanded", "true");
+      nav.setAttribute("data-open", "true");
+      open = true;
+    } else {
+      this.setAttribute("aria-expanded", "false");
+      nav.setAttribute("data-open", "false");
+      open = false;
     }
+  }
 })();
