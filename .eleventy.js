c7b333e02a863b80cd64774c3f5af0bae7b1ffa2
Sara Soueidan <sara.soueidan@gmail.com>
2021-12-07 09:56:25 +0200

    fix: remove moment dependency  (#29)
    * remove moment dependency and cleanup other eleventy config code
    
    * remove TOC plugin
    
    * remove toc requirement from eleventy.js file

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,7 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const pluginTOC = require('eleventy-plugin-nesting-toc');
 const readingTime = require('eleventy-plugin-reading-time');
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const moment = require("moment");
 
 const {
     DateTime
@@ -23,8 +21,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter('jsonify', function(variable) {
         return JSON.stringify(variable);
     });
-    /* order collection by the order specified in the front matter (useful for course content)
-       hat tip: https://www.martinjc.com/blog/posts/2020-10-19-course-notes-with-eleventy/ */
+    /* order collection by the order specified in the front matter */
     eleventyConfig.addFilter("sortByPageOrder", function(values) {
         return values.slice().sort((a, b) => a.data.order - b.data.order);
     });
@@ -41,44 +38,6 @@ module.exports = function(eleventyConfig) {
         return DateTime.fromISO(ISODate).toUTC().toLocaleString(DateTime.DATE_FULL);
     });
 
-    eleventyConfig.addFilter("past", function(date) {
-        var todayDate = moment();
-        postDate = moment(date, "YYYY-MM-DD");
-        return todayDate.isAfter(postDate);
-    });
-    eleventyConfig.addFilter("future", function(date) {
-        var todayDate = moment();
-        postDate = moment(date, "YYYY-MM-DD");
-        return !todayDate.isAfter(postDate);
-    });
-
-    // squash. Source: https://github.com/philhawksworth/hawksworx.com/blob/8c96ba2541c8fd6fe6f521cdb5e17848c231636c/src/site/_filters/squash.js
-    // info: https://www.hawksworx.com/blog/adding-search-to-a-jamstack-site/
-    eleventyConfig.addFilter("squash", function(text) {
-        var content = new String(text);
-
-        // all lower case, please
-        var content = content.toLowerCase();
-
-        // remove all html elements and new lines
-        var re = /(&lt;.*?&gt;)/gi;
-        var plain = unescape(content.replace(re, ''));
-
-        // remove duplicated words
-        var words = plain.split(' ');
-        var deduped = [...(new Set(words))];
-        var dedupedStr = deduped.join(' ')
-
-        // remove short and less meaningful words
-        var result = dedupedStr.replace(/\b(\.|\,|the|a|an|and|am|you|I|to|if|of|off|me|my|on|in|it|is|at|as|we|do|be|has|but|was|so|no|not|or|up|for)\b/gi, '');
-        //remove newlines, and punctuation
-        result = result.replace(/\.|\,|\?|-|—|\n/g, '');
-        //remove repeated spaces
-        result = result.replace(/[ ]{2,}/g, ' ');
-
-        return result;
-    })
-
     eleventyConfig.addFilter('dollars', value => {
         return new Intl.NumberFormat("en-US", {
             style: "currency",
@@ -93,13 +52,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(readingTime);
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPlugin(pluginRss);
-    eleventyConfig.addPlugin(pluginTOC, {
-        tags: ['h2', 'h3', 'h4'],
-        wrapper: 'nav', // Element to put around the root `ol`
-        wrapperClass: 'c-toc', // Class for the element around the root `ol`
-        headingText: '', // Optional text to show in heading above the wrapper element
-        headingTag: 'h2' // Heading tag when showing heading above the wrapper element
-    });
     // add IDs to the headers
     const markdownIt = require('markdown-it');
     const markdownItAnchor = require('markdown-it-anchor');
