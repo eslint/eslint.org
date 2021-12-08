4b75db2861e7c4c7ba2880903de37dfbe0ab1386
Sara Soueidan <sara.soueidan@gmail.com>
2021-12-08 14:40:22 +0200

    add categories [WIP]

--- .eleventy.js
+++ .eleventy.js
@@ -108,6 +108,23 @@ module.exports = function(eleventyConfig) {
         }).reverse();
     });
 
+    eleventyConfig.addCollection(
+    "blogposts",
+    require("./src/_11ty/collections/blogposts.js")
+  );
+
+  // blogposts unique categories
+  eleventyConfig.addCollection(
+    "blogCategories",
+    require("./src/_11ty/collections/blogpostsCategories.js")
+  );
+
+  // blogposts by categories
+  eleventyConfig.addCollection(
+    "blogpostsByCategories",
+    require("./src/_11ty/collections/blogpostsByCategories.js")
+  );
+
     /*****************************************************************************************
      *  Shortcodes
      * ***************************************************************************************/
