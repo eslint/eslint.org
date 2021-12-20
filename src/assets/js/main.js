8cda63b8de8e4d66d123b68642f9e9810b4668a8
Sara Soueidan <sara.soueidan@gmail.com>
2021-12-20 19:07:58 +0200

    remove alert js from main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -24,44 +24,7 @@
   }
 })();
 
-
-/* ALERTS */
-
-/* element.closest() polyfill */
-
-// if (!Element.prototype.matches) {
-//   Element.prototype.matches =
-//     Element.prototype.msMatchesSelector ||
-//     Element.prototype.webkitMatchesSelector;
-// }
-
-// if (!Element.prototype.closest) {
-//   Element.prototype.closest = function(s) {
-//     var el = this;
-
-//     do {
-//       if (Element.prototype.matches.call(el, s)) return el;
-//       el = el.parentElement || el.parentNode;
-//     } while (el !== null && el.nodeType === 1);
-//     return null;
-//   };
-// }
-
-
-// function removeParent(e) {
-//     parent = e.target.closest('.alert');
-//     parent.remove();
-// }
-
-// (function() {
-//   var alert_btn = document.querySelectorAll('.alert__remove-btn');
-
-//     alert_btn.forEach((button) => {
-//         button.removeAttribute('hidden');
-//         button.addEventListener('click', removeParent, false);
-//     })
-
-// })();
+/* branding logo theme switcher */
 (function(){
     var logo_theme_buttons = document.querySelector('.brand__logo__colors'),
         logo_theme_btn = document.querySelectorAll('.brand__logo__colors__btn'),
