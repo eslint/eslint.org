e3038e8668d30925ce182bfe842c635279857030
Sara Soueidan <sara.soueidan@gmail.com>
2021-12-29 06:28:53 +0200

    feat: Languages switcher (#48)
    * create global site config file
    
    * add languages page
    
    * add languages list partial
    
    * add languages styles
    
    * separate language switcher into own partial
    
    * add languages data file
    
    * set page and language locales
    
    * add switcher markup
    
    * add custom select styles
    
    * style language switcher
    
    * housekeeping
    
    * remove alert js from main.js
    
    * add behavior to switcher
    
    * housekeeping
    
    * rename lang code and add switcher to component library page
    
    * fix chinese url
    
    * add click listener
    
    * fix select click behavio and align label on mobile

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,8 @@
 (function() {
-  var nav_trigger = document.getElementById("nav-toggle"),
-    nav = document.getElementById("nav-list"),
-    body = document.getElementsByTagName("body")[0],
-    open = false;
+    var nav_trigger = document.getElementById("nav-toggle"),
+        nav = document.getElementById("nav-list"),
+        body = document.getElementsByTagName("body")[0],
+        open = false;
 
     nav_trigger.removeAttribute("hidden");
     nav_trigger.setAttribute("aria-expanded", "false");
@@ -11,75 +11,69 @@
     nav.setAttribute("data-open", "false");
     nav_trigger.addEventListener("click", togglenav, false);
 
-  function togglenav(e) {
-    if (!open) {
-      this.setAttribute("aria-expanded", "true");
-      nav.setAttribute("data-open", "true");
-      open = true;
-    } else {
-      this.setAttribute("aria-expanded", "false");
-      nav.setAttribute("data-open", "false");
-      open = false;
+    function togglenav(e) {
+        if (!open) {
+            this.setAttribute("aria-expanded", "true");
+            nav.setAttribute("data-open", "true");
+            open = true;
+        } else {
+            this.setAttribute("aria-expanded", "false");
+            nav.setAttribute("data-open", "false");
+            open = false;
+        }
     }
-  }
 })();
 
-
-/* ALERTS */
-
-/* element.closest() polyfill */
-
-// if (!Element.prototype.matches) {
-//   Element.prototype.matches =
-//     Element.prototype.msMatchesSelector ||
-//     Element.prototype.webkitMatchesSelector;
-// }
-
-// if (!Element.prototype.closest) {
-//   Element.prototype.closest = function(s) {
-//     var el = this;
-
-//     do {
-//       if (Element.prototype.matches.call(el, s)) return el;
-//       el = el.parentElement || el.parentNode;
-//     } while (el !== null && el.nodeType === 1);
-//     return null;
-//   };
-// }
-
-
-// function removeParent(e) {
-//     parent = e.target.closest('.alert');
-//     parent.remove();
-// }
-
-// (function() {
-//   var alert_btn = document.querySelectorAll('.alert__remove-btn');
-
-//     alert_btn.forEach((button) => {
-//         button.removeAttribute('hidden');
-//         button.addEventListener('click', removeParent, false);
-//     })
-
-// })();
-(function(){
+/* branding logo theme switcher */
+(function() {
     var logo_theme_buttons = document.querySelector('.brand__logo__colors'),
         logo_theme_btn = document.querySelectorAll('.brand__logo__colors__btn'),
         logo_container = document.querySelector('.brand__logo__img');
 
-    logo_theme_buttons.removeAttribute('hidden');
-    logo_theme_btn.forEach((button) => {
+    if (logo_theme_buttons != null) {
+        logo_theme_buttons.removeAttribute('hidden');
+        logo_theme_btn.forEach((button) => {
 
-        button.addEventListener('click', function() {
-            btn_theme = button.getAttribute('data-theme');
-            unsetOtherButtons();
-            button.setAttribute('aria-pressed', 'true');
-            logo_container.setAttribute('data-theme', btn_theme);
+            button.addEventListener('click', function() {
+                btn_theme = button.getAttribute('data-theme');
+                unsetOtherButtons();
+                button.setAttribute('aria-pressed', 'true');
+                logo_container.setAttribute('data-theme', btn_theme);
+            })
         })
-    })
+    }
 
     function unsetOtherButtons() {
         currentButton = document.querySelector('.brand__logo__colors__btn[aria-pressed="true"]');
         currentButton.setAttribute('aria-pressed', "false");
     }
+})();
+
+/* switchers */
+
+(function() {
+    var switchers = document.querySelectorAll('.switcher'),
+        fallbacks = document.querySelectorAll('.switcher-fallback');
+
+    if(fallbacks != null) {
+        fallbacks.forEach(el => {
+            el.setAttribute('hidden', '');
+        });
+    }
+
+    if (switchers != null) {
+        switchers.forEach(element => {
+            element.removeAttribute('hidden');
+            const select = element.querySelector('select');
+
+            select.addEventListener('change', function() {
+                var selected = this.options[this.selectedIndex];
+                url = selected.getAttribute('data-url');
+
+                window.location.href =  url;
+            })
+        });
+    }
+
+
 })();
