439c0021e3bbc9d9cdde21b7364e52fb83c8475e
Sara Soueidan <sara.soueidan@gmail.com>
2022-01-03 19:49:14 +0200

    move themes js into render-blocking file

--- src/assets/js/themes.js
+++ src/assets/js/themes.js
@@ -1,53 +1,58 @@
-// /* theme toggle buttons */
-// (function() {
-//     var switcher = document.getElementById('js-theme-switcher');
-//     switcher.removeAttribute('hidden');
+/* theme toggle buttons */
+(function() {
+    var enableToggle = function(btn) {
+        btn.setAttribute("aria-pressed", "true");
+    }
 
-//     var light_theme_toggle = document.getElementById('light-theme-toggle'),
-//         dark_theme_toggle = document.getElementById('dark-theme-toggle'),
-//         theme = window.localStorage.getItem("theme") || 'light';
+    var disableToggle = function(btn) {
+        btn.setAttribute("aria-pressed", "false");
+    }
 
-//     var enableToggle = function(btn) {
-//         btn.setAttribute("aria-pressed", "true");
-//     }
+    var switcher = document.getElementById('js-theme-switcher');
+    let theme = window.localStorage.getItem("theme");
+    document.documentElement.setAttribute('data-theme', theme);
 
-//     var disableToggle = function(btn) {
-//         btn.setAttribute("aria-pressed", "false");
-//     }
+    if(!theme) document.documentElement.setAttribute('data-theme', 'light');
 
-//     document.documentElement.setAttribute('data-theme', theme);
+    if(switcher !== null) {
+        switcher.removeAttribute('hidden');
+        var light_theme_toggle = document.getElementById('light-theme-toggle'),
+            dark_theme_toggle = document.getElementById('dark-theme-toggle');
 
-//     if (theme == "light") {
-//         enableToggle(light_theme_toggle);
-//         disableToggle(dark_theme_toggle);
-//     } else if (theme == "dark") {
-//         enableToggle(dark_theme_toggle);
-//         disableToggle(light_theme_toggle);
-//     }
 
-//     // if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//     //     document.documentElement.setAttribute('data-theme', 'dark');
-//     // }
+        if(theme == "light") {
+            enableToggle(light_theme_toggle);
+            disableToggle(dark_theme_toggle);
+        }
+        else if(theme ==  "dark") {
+            enableToggle(dark_theme_toggle);
+            disableToggle(light_theme_toggle);
+        }
 
-//     // window.localStorage.setItem("theme", theme);
-//     // var theme = window.localStorage.getItem("theme");
+    // if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    //     document.documentElement.setAttribute('data-theme', 'dark');
+    // }
 
-//     light_theme_toggle.addEventListener("click", function() {
-//         enableToggle(light_theme_toggle);
-//         theme = this.getAttribute('data-theme');
-//         document.documentElement.setAttribute('data-theme', theme);
-//         window.localStorage.setItem("theme", theme);
+    // window.localStorage.setItem("theme", theme);
+    // var theme = window.localStorage.getItem("theme");
 
-//         disableToggle(dark_theme_toggle);
-//     }, false);
+        light_theme_toggle.addEventListener("click", function() {
+            enableToggle(light_theme_toggle);
+            theme = this.getAttribute('data-theme');
+            document.documentElement.setAttribute('data-theme', theme);
+            window.localStorage.setItem("theme", theme);
 
-//     dark_theme_toggle.addEventListener("click", function() {
-//         enableToggle(dark_theme_toggle);
-//         theme = this.getAttribute('data-theme');
-//         document.documentElement.setAttribute('data-theme', theme);
-//         window.localStorage.setItem("theme", theme);
+            disableToggle(dark_theme_toggle);
+        }, false);
 
-//         disableToggle(light_theme_toggle);
-//     }, false);
+        dark_theme_toggle.addEventListener("click", function() {
+            enableToggle(dark_theme_toggle);
+            theme = this.getAttribute('data-theme');
+            document.documentElement.setAttribute('data-theme', theme);
+            window.localStorage.setItem("theme", theme);
 
-// })();
+            disableToggle(light_theme_toggle);
+        }, false);
+    }
+
+})();
