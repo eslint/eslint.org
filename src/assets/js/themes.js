5d9042b0cfdd2535c7428ff13505c066789a1af5
Sara Soueidan <sara.soueidan@gmail.com>
2022-01-13 19:28:08 +0200

    feat: set theme behavior to respect OS prefs by default (#78)
    * feat: set theme behavior to respect OS prefs by default
    
    * slightly avoid theme FOUC
    
    * undo docs ignore

--- src/assets/js/themes.js
+++ src/assets/js/themes.js
@@ -15,14 +15,17 @@
 
     document.addEventListener('DOMContentLoaded', function() {
         var switcher = document.getElementById('js-theme-switcher');
-
-
-
-
         switcher.removeAttribute('hidden');
+
         var light_theme_toggle = document.getElementById('light-theme-toggle'),
             dark_theme_toggle = document.getElementById('dark-theme-toggle');
 
+        // get any previously-chosen themes
+        var theme = window.localStorage.getItem("theme");
+        if (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
+        else if (theme) document.documentElement.setAttribute('data-theme', theme);
 
         if (theme == "light") {
             enableToggle(light_theme_toggle);
@@ -31,13 +34,6 @@
             enableToggle(dark_theme_toggle);
             disableToggle(light_theme_toggle);
         }
-
-        // if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        //     document.documentElement.setAttribute('data-theme', 'dark');
-        // }
-
-        // window.localStorage.setItem("theme", theme);
-        // var theme = window.localStorage.getItem("theme");
 
         light_theme_toggle.addEventListener("click", function() {
             enableToggle(light_theme_toggle);
