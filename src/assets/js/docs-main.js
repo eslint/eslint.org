9508f5fb8625463cfefc39275d0445e9932d8e5c
Sara Soueidan <sara.soueidan@gmail.com>
2022-01-14 10:59:13 +0200

    separate docs main.js

---
+++ src/assets/js/docs-main.js
@@ -0,0 +1,51 @@
+(function() {
+    var nav_trigger = document.getElementById("nav-toggle"),
+        nav = document.getElementById("nav-list"),
+        body = document.getElementsByTagName("body")[0],
+        open = false;
+
+    nav_trigger.removeAttribute("hidden");
+    nav_trigger.setAttribute("aria-expanded", "false");
+    nav.setAttribute("data-open", "false");
+
+    nav.setAttribute("data-open", "false");
+    nav_trigger.addEventListener("click", togglenav, false);
+
+    function togglenav(e) {
+        if (!open) {
+            this.setAttribute("aria-expanded", "true");
+            nav.setAttribute("data-open", "true");
+            open = true;
+        } else {
+            this.setAttribute("aria-expanded", "false");
+            nav.setAttribute("data-open", "false");
+            open = false;
+        }
+    }
+})();
+
+(function() {
+    var index_trigger = document.getElementById("index-toggle"),
+        index = document.getElementById("js-index-list"),
+        body = document.getElementsByTagName("body")[0],
+        open = false;
+
+    index_trigger.removeAttribute("hidden");
+    index_trigger.setAttribute("aria-expanded", "false");
+    index.setAttribute("data-open", "false");
+
+    index.setAttribute("data-open", "false");
+    index_trigger.addEventListener("click", toggleindex, false);
+
+    function toggleindex(e) {
+        if (!open) {
+            this.setAttribute("aria-expanded", "true");
+            index.setAttribute("data-open", "true");
+            open = true;
+        } else {
+            this.setAttribute("aria-expanded", "false");
+            index.setAttribute("data-open", "false");
+            open = false;
+        }
+    }
+})();
