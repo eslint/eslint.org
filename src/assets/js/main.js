6cc82836b0efed508c2c39edecce444a818c52cb
Sara Soueidan <sara.soueidan@gmail.com>
2022-02-10 07:56:14 +0200

    make component library index collapsible

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -38,6 +38,44 @@
     }
 })();
 
+(function() {
+    var index_trigger = document.getElementById("js-index-toggle"),
+        index = document.getElementById("js-index-list"),
+        body = document.getElementsByTagName("body")[0],
+        open = false;
+
+    if (index_trigger && index && matchMedia) {
+        const mq = window.matchMedia("(max-width: 1023px)");
+        mq.addListener(WidthChange);
+        WidthChange(mq);
+    }
+
+    function WidthChange(mq) {
+        initIndex();
+    }
+
+    function initIndex() {
+        index_trigger.removeAttribute("hidden");
+        index_trigger.setAttribute("aria-expanded", "false");
+        index.setAttribute("data-open", "false");
+
+        index.setAttribute("data-open", "false");
+        index_trigger.addEventListener("click", toggleindex, false);
+
+        function toggleindex(e) {
+            if (!open) {
+                this.setAttribute("aria-expanded", "true");
+                index.setAttribute("data-open", "true");
+                open = true;
+            } else {
+                this.setAttribute("aria-expanded", "false");
+                index.setAttribute("data-open", "false");
+                open = false;
+            }
+        }
+    }
+})();
+
 
 /* branding logo theme switcher */
 (function() {
