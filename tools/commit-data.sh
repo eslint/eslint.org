4bcdcbda7715b00f5392b9c6fc8671d3f3ba4cd6
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2022-03-16 11:23:10 -0700

    chore: Automatically fetch data each night

---
+++ tools/commit-data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Commits the data files if any have changed
+#------------------------------------------------------------------------------
+
+if [ -z "$(git status --porcelain)" ]; then 
+	echo "Data did not change."
+else
+	echo "Data changed!"
+
+	# commit the result
+	git add _data/
+	git commit -m "chore: Update remote data"
+
+	# push back to source control
+	git push origin HEAD  
+fi
