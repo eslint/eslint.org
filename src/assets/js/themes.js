cc53724d80a2200a724e3756c38e7e06b1482fc1
amaresh.sm <amareshsm13@gmail.com>
2022-05-07 16:18:02 +0530

    chore: convert to es5 & condition change

--- src/assets/js/themes.js
+++ src/assets/js/themes.js
@@ -1,26 +1,26 @@
 /* theme toggle buttons */
 (function () {
-    const enableToggle = btn => {
+    var enableToggle = function (btn) {
         btn.setAttribute("aria-pressed", "true");
     }
-    const disableToggle = btn => {
+    var disableToggle = function (btn) {
         btn.setAttribute("aria-pressed", "false");
     }
-    const setTheme = theme => {
+    var setTheme = function (theme) {
         document.documentElement.setAttribute('data-theme', theme);
         window.localStorage.setItem("theme", theme);
     }
 
-    let theme = window.localStorage.getItem("theme");
-    if (!theme || !(theme === "light") || !(theme === "dark")) {
-        let isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    var theme = window.localStorage.getItem("theme");
+    if (!theme || (theme !== "light" && theme !== "dark")) {
+        var isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
         theme = isDark ? "dark" : "light";
     }
 
     document.addEventListener('DOMContentLoaded', function () {
-        const switcher = document.getElementById('js-theme-switcher');
+        var switcher = document.getElementById('js-theme-switcher');
         switcher.removeAttribute('hidden');
-        const light_theme_toggle = document.getElementById('light-theme-toggle'),
+        var light_theme_toggle = document.getElementById('light-theme-toggle'),
             dark_theme_toggle = document.getElementById('dark-theme-toggle');
         if (theme === "light") {
             enableToggle(light_theme_toggle);
