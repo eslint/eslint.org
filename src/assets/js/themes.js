8af43bf528445f6137b3cac3343917311f528921
Amaresh  S M <amareshsm13@gmail.com>
2022-05-11 02:45:10 +0530

    fix: set default theme (#208)
    * fix: set default theme
    
    Default theme is not set both light and dark theme animated dropdowns are shown.
    
    * chore: indented the code
    
    * chore: convert to es5 & condition change
    
    * fix: update store only if theme value falsy

--- src/assets/js/themes.js
+++ src/assets/js/themes.js
@@ -1,49 +1,51 @@
 /* theme toggle buttons */
-(function() {
-    var enableToggle = function(btn) {
+(function () {
+    var enableToggle = function (btn) {
         btn.setAttribute("aria-pressed", "true");
     }
-
-    var disableToggle = function(btn) {
+    var disableToggle = function (btn) {
         btn.setAttribute("aria-pressed", "false");
     }
+    var setTheme = function (theme) {
+        document.documentElement.setAttribute('data-theme', theme);
+        window.localStorage.setItem("theme", theme);
+    }
 
+    var theme = window.localStorage.getItem("theme");
+    if (!theme || (theme !== "light" && theme !== "dark")) {
+        var isChanged = true;
+        var isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        theme = isDark ? "dark" : "light";
+    }
 
-    let theme = window.localStorage.getItem("theme");
-
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         var switcher = document.getElementById('js-theme-switcher');
         switcher.removeAttribute('hidden');
-
         var light_theme_toggle = document.getElementById('light-theme-toggle'),
             dark_theme_toggle = document.getElementById('dark-theme-toggle');
-
-
-        if (theme == "light") {
-            enableToggle(light_theme_toggle);
-            disableToggle(dark_theme_toggle);
-        } else if (theme == "dark") {
-            enableToggle(dark_theme_toggle);
-            disableToggle(light_theme_toggle);
+        if (isChanged) {
+            if (theme === "light") {
+                enableToggle(light_theme_toggle);
+                disableToggle(dark_theme_toggle);
+                setTheme(theme);
+            } else if (theme === "dark") {
+                enableToggle(dark_theme_toggle);
+                disableToggle(light_theme_toggle);
+                setTheme(theme);
+            }
         }
-
-        light_theme_toggle.addEventListener("click", function() {
+        light_theme_toggle.addEventListener("click", function () {
             enableToggle(light_theme_toggle);
             theme = this.getAttribute('data-theme');
-            document.documentElement.setAttribute('data-theme', theme);
-            window.localStorage.setItem("theme", theme);
-
+            setTheme(theme);
             disableToggle(dark_theme_toggle);
-        }, false);
 
-        dark_theme_toggle.addEventListener("click", function() {
+        }, false);
+        dark_theme_toggle.addEventListener("click", function () {
             enableToggle(dark_theme_toggle);
             theme = this.getAttribute('data-theme');
-            document.documentElement.setAttribute('data-theme', theme);
-            window.localStorage.setItem("theme", theme);
-
+            setTheme(theme);
             disableToggle(light_theme_toggle);
         }, false);
     }, false);
-
 })();
