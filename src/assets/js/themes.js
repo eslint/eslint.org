a218f8b0a746532461646859d6b2459921d101d0
amaresh.sm <amareshsm13@gmail.com>
2022-05-11 10:18:20 +0530

    fix: highlight selected theme
    Highlighted selected theme.

--- src/assets/js/themes.js
+++ src/assets/js/themes.js
@@ -12,26 +12,18 @@
     }
 
     var theme = window.localStorage.getItem("theme");
-    if (!theme || (theme !== "light" && theme !== "dark")) {
-        var isChanged = true;
-        var isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        theme = isDark ? "dark" : "light";
-    }
-
     document.addEventListener('DOMContentLoaded', function () {
         var switcher = document.getElementById('js-theme-switcher');
         switcher.removeAttribute('hidden');
         var light_theme_toggle = document.getElementById('light-theme-toggle'),
             dark_theme_toggle = document.getElementById('dark-theme-toggle');
-        if (isChanged) {
+        if (theme) {
             if (theme === "light") {
                 enableToggle(light_theme_toggle);
                 disableToggle(dark_theme_toggle);
-                setTheme(theme);
             } else if (theme === "dark") {
                 enableToggle(dark_theme_toggle);
                 disableToggle(light_theme_toggle);
-                setTheme(theme);
             }
         }
         light_theme_toggle.addEventListener("click", function () {
@@ -39,7 +31,6 @@
             theme = this.getAttribute('data-theme');
             setTheme(theme);
             disableToggle(dark_theme_toggle);
-
         }, false);
         dark_theme_toggle.addEventListener("click", function () {
             enableToggle(dark_theme_toggle);
