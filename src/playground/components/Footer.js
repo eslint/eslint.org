0d6e22a87b8d613418ae6b67206e3da327abea39
Nitin Kumar <snitin315@gmail.com>
2022-07-16 13:36:02 +0530

    fix: footer styles

--- src/playground/components/Footer.js
+++ src/playground/components/Footer.js
@@ -4,7 +4,7 @@ import SocialIcons from "./SocialIcons";
 
 export default function Footer() {
     return (
-        <footer className="footer">
+        <footer className="playground-footer" >
             <SocialIcons/>
             <div className="copyright">
                 &copy; OpenJS Foundation and other contributors, <a href="https://www.openjsf.org">www.openjsf.org</a>
