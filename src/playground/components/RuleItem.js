ce2cf8d70a1934114143d03a11250f8b1db4df49
Shanmughapriyan S <priyanshan03@gmail.com>
2022-08-03 18:54:46 +0530

    refactor: use function keyword

--- src/playground/components/RuleItem.js
+++ src/playground/components/RuleItem.js
@@ -1,10 +1,5 @@
 import React from "react";
 
-const RuleItem = ({ children }) =>
-    (
-        <li className="config__added-rules__item">
-            {children}
-        </li>
-    );
-
-export default RuleItem;
+export default function RuleItem({ children }) {
+    return <li className="config__added-rules__item">{children}</li>;
+}
