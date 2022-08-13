276f9363d20cccdebd3efbd33566ae360203bc00
Strek <ssharishkumar@gmail.com>
2022-08-13 20:45:05 +0530

    Revert "chore: fix build"
    This reverts commit 061df8c8afe81f7f23b441895d46a609e4696fb5.

--- src/playground/components/CodeEditor.js
+++ src/playground/components/CodeEditor.js
@@ -5,7 +5,7 @@ import { bracketMatching } from "@codemirror/matchbrackets";
 import { javascript, esLint } from "@codemirror/lang-javascript";
 import { linter } from "../utils/codemirror-linter-extension";
 import { ESLintPlaygroundTheme, ESLintPlaygroundHighlightStyle } from "../utils/codemirror-theme";
-import { Linter as ESLint } from "../../../node_modules/eslint/lib/linter/";
+import { Linter as ESLint } from "../node_modules/eslint/lib/linter/";
 import "../scss/editor.scss";
 
 export default function CodeEditor({ codeValue, onUpdate, eslintOptions }) {
