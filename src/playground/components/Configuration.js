a99113a660bf55987fc3a92a02a27a18b889e34f
Strek <ssharishkumar@gmail.com>
2022-11-26 20:55:06 +0530

    chore: add keyboard for multiselect

--- src/playground/components/Configuration.js
+++ src/playground/components/Configuration.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import Select from "react-select";
+import Select, { components } from "react-select";
 import RuleList from "./RuleList";
 import ShareURL from "./ShareURL";
 import { ECMA_FEATURES, ECMA_VERSIONS, SOURCE_TYPES, ENV_NAMES } from "../utils/constants";
@@ -91,10 +91,35 @@ export default function Configuration({ rulesMeta, eslintVersion, onUpdate, opti
     const ruleInputRef = useRef(null);
     const [rulesWithInvalidConfigs, setRulesWithInvalidConfigs] = useState(new Set([]));
 
+    const handleRuleChange = () => {
+        selectedRules.forEach(selectedRule => {
+            if (ruleNames.includes(selectedRule)) {
+                options.rules[selectedRule] = ["error"];
+                onUpdate(Object.assign({}, options));
+                ruleInputRef.current.setValue("");
+            }
+        });
+    };
+
+    const Input = props => {
+        if (props.isHidden) {
+            return <components.Input {...props} />;
+        }
+        return (
+            <components.Input {...props} onKeyDown={e => {
+                if ((e.metaKey || e.ctrlKey) && e.code === "Enter") {
+                    handleRuleChange();
+                } else {
+                    props.onKeyDown(e);
+                }
+            }} />
+        );
+    };
+
     return (
         <div className="playground__config-options__sections">
             <div className="playground__config-options__section">
-                <ShareURL url={window.location}/>
+                <ShareURL url={window.location} />
             </div>
             <div className="playground__config-options__section">
                 <h2 data-config-section-title className="title-toggle">Versioning and Config</h2>
@@ -125,7 +150,7 @@ export default function Configuration({ rulesMeta, eslintVersion, onUpdate, opti
                             isSearchable={false}
                             styles={customStyles}
                             theme={theme => customTheme(theme)}
-                            defaultValue={sourceTypeOptions.filter(sourceTypeOption => options.parserOptions.sourceType === sourceTypeOption.value) }
+                            defaultValue={sourceTypeOptions.filter(sourceTypeOption => options.parserOptions.sourceType === sourceTypeOption.value)}
                             options={sourceTypeOptions}
                             onChange={selected => {
                                 options.parserOptions.sourceType = selected.value;
@@ -180,6 +205,7 @@ export default function Configuration({ rulesMeta, eslintVersion, onUpdate, opti
                     <label htmlFor="rules" className="combo-label"><span className="label__text">Choose rules</span></label>
                     <Select
                         isMulti
+                        components={{ Input }}
                         isClearable
                         isSearchable
                         styles={customStyles}
@@ -197,14 +223,7 @@ export default function Configuration({ rulesMeta, eslintVersion, onUpdate, opti
                     <button
                         className="c-btn c-btn--ghost add-rule-btn"
                         onClick={() => {
-                            selectedRules.forEach(selectedRule => {
-                                if (ruleNames.includes(selectedRule)) {
-                                    options.rules[selectedRule] = ["error"];
-                                    onUpdate(Object.assign({}, options));
-                                    ruleInputRef.current.setValue("");
-                                }
-                            });
-
+                            handleRuleChange();
                         }}
                     >
                         {selectedRules.length > 1 ? "Add these Rules" : "Add this Rule"}
@@ -226,7 +245,7 @@ export default function Configuration({ rulesMeta, eslintVersion, onUpdate, opti
                                         }}
                                     >
                                         <svg width="25" height="25" viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M28.5 16L16.5 28M16.5 16L28.5 28" stroke="var(--link-color)" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round"/>
+                                            <path d="M28.5 16L16.5 28M16.5 16L28.5 28" stroke="var(--link-color)" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round" />
                                         </svg>
                                     </button>
                                 </h4>
