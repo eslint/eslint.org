53721e92963158fbd941d85ebbc4bf7063485c5b
Nitin Kumar <snitin315@gmail.com>
2023-03-02 03:17:34 +0530

    docs: detect and fix broken links (#416)
    * docs: detect and fix broken links
    
    * ci: fix
    
    * ci: fix
    
    * chore: use custom script
    
    * chore: fix syntax error
    
    * chore: fix script
    
    * chore: remove unwanted changes
    
    * ci: test script on windows
    
    * ci: test script on windows
    
    * ci: use ubuntu runner
    
    * ci: uncomment steps

---
+++ tools/validate-links.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const TapRender = require("@munter/tap-render");
+const spot = require("tap-spot");
+const hyperlink = require("hyperlink");
+
+const tapRenderInstance = new TapRender();
+
+tapRenderInstance.pipe(spot()).pipe(process.stdout);
+
+const skipPatterns = [
+    "https://",
+    "fragment-redirect",
+    path.normalize("_site/sponsors"),
+    "/docs"
+];
+
+const skipFilter = (report) =>
+    Object.values(report).some((value) =>
+        skipPatterns.some((pattern) => String(value).includes(pattern))
+    );
+
+(async () => {
+    try {
+        await hyperlink(
+            {
+                inputUrls: ["../_site/index.html"],
+                root: path.resolve(__dirname, "../_site"),
+                canonicalRoot: "https://eslint.org/",
+                recursive: true,
+                internalOnly: true,
+                pretty: true,
+                concurrency: 25,
+                skipFilter,
+            },
+            tapRenderInstance
+        );
+    } catch (err) {
+        console.log(err.stack);
+        process.exit(1);
+    }
+    const results = tapRenderInstance.close();
+
+    process.exit(results.fail ? 1 : 0);
+})();
