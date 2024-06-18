fe930e6eb66a4cbf4aa92dcb9a4a418043557505
Mohammad <smadi.dev@gmail.com>
2024-06-18 16:28:53 +0300

    chore: Cleaned up eslint/prettier configurations and updated lint-staged scripts and ci.yml

--- eslint.config.js
+++ eslint.config.js
@@ -12,103 +12,99 @@ const { fixupPluginRules } = require("@eslint/compat");
 const playgroundFiles = "src/playground/**/*.{js,jsx}";
 
 module.exports = [
-    {
-        ignores: [
-            "_site/**",
-            "src/assets/js/**",
-            "src/_data/**",
-            "src/_11ty/**"
-        ]
-    },
+	{
+		ignores: [
+			"_site/**",
+			"src/assets/js/**",
+			"src/_data/**",
+			"src/_11ty/**",
+		],
+	},
 
-    ...eslintConfigESLintCJS.map(config => ({
-        ...config,
-        ignores: [playgroundFiles]
-    })),
-    {
-        rules: {
-            "n/no-extraneous-require": ["error", {
-                allowModules: ["luxon"]
-            }]
-        },
-        ignores: [playgroundFiles]
-    },
+	...eslintConfigESLintCJS.map(config => ({
+		...config,
+		ignores: [playgroundFiles],
+	})),
+	{
+		rules: {
+			"n/no-extraneous-require": [
+				"error",
+				{
+					allowModules: ["luxon"],
+				},
+			],
+		},
+		ignores: [playgroundFiles],
+	},
 
-    {
-        files: ["tools/**/*.js"],
-        rules: {
-            "no-console": "off",
-            "n/no-process-exit": "off"
-        }
-    },
+	{
+		files: ["tools/**/*.js"],
+		rules: {
+			"no-console": "off",
+			"n/no-process-exit": "off",
+		},
+	},
 
-    // Playground
-    ...eslintConfigESLintBase.map(config => ({
-        ...config,
-        files: [playgroundFiles]
-    })),
-    {
-        files: [playgroundFiles],
-        plugins: {
-            react: fixupPluginRules(reactPlugin),
-            "jsx-a11y": fixupPluginRules(jsxA11yPlugin),
-            "react-hooks": fixupPluginRules(reactHooksPlugin)
-        },
-        settings: {
-            react: {
-                version: "16.8.6"
-            }
-        },
-        languageOptions: {
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true
-                }
-            },
-            sourceType: "module",
-            globals: {
-                ...globals.browser,
-                ...globals.es2021
-            }
-        },
-        rules: {
-            ...reactRecommended.rules,
-            ...jsxA11yPlugin.configs.recommended.rules,
-            "react/jsx-no-useless-fragment": "error",
-            "react/jsx-no-target-blank": "error",
+	// Playground
+	...eslintConfigESLintBase.map(config => ({
+		...config,
+		files: [playgroundFiles],
+	})),
+	{
+		files: [playgroundFiles],
+		plugins: {
+			react: fixupPluginRules(reactPlugin),
+			"jsx-a11y": fixupPluginRules(jsxA11yPlugin),
+			"react-hooks": fixupPluginRules(reactHooksPlugin),
+		},
+		settings: {
+			react: {
+				version: "16.8.6",
+			},
+		},
+		languageOptions: {
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+			sourceType: "module",
+			globals: {
+				...globals.browser,
+				...globals.es2021,
+			},
+		},
+		rules: {
+			...reactRecommended.rules,
+			...jsxA11yPlugin.configs.recommended.rules,
+			"react/jsx-no-useless-fragment": "error",
+			"react/jsx-no-target-blank": "error",
 
-            // Disable rules that the codebase doesn't currently follow.
-            // It might be a good idea to enable these in the future.
-            "jsx-a11y/no-onchange": "off",
-            "react/prop-types": "off",
-            "jsdoc/require-jsdoc": "off",
-            "react-hooks/rules-of-hooks": "error",
-            "react-hooks/exhaustive-deps": "warn",
-            "func-style": "off"
-        }
-    },
+			// Disable rules that the codebase doesn't currently follow.
+			// It might be a good idea to enable these in the future.
+			"jsx-a11y/no-onchange": "off",
+			"react/prop-types": "off",
+			"jsdoc/require-jsdoc": "off",
+			"react-hooks/rules-of-hooks": "error",
+			"react-hooks/exhaustive-deps": "warn",
+			"func-style": "off",
+		},
+	},
 
-    // Disable rules that the codebase doesn't currently follow.
-    // It might be a good idea to enable these in the future.
-    {
-        rules: {
-            "jsdoc/require-jsdoc": "off",
-            "jsdoc/require-returns": "off",
-            "jsdoc/require-param-description": "off",
-            "jsdoc/require-param-type": "off",
-            "jsdoc/no-bad-blocks": ["error", {
-                ignore: ["__PURE__"]
-            }]
-        }
-    },
-
-    // Disable rules that prettier will handle.
-    {
-        rules: {
-            "no-tabs": "off",
-            indent: "off",
-            "lines-around-comment": "off",
-            "function-paren-newline": "off"
-        }
-    }
+	// Disable rules that the codebase doesn't currently follow.
+	// It might be a good idea to enable these in the future.
+	{
+		rules: {
+			"jsdoc/require-jsdoc": "off",
+			"jsdoc/require-returns": "off",
+			"jsdoc/require-param-description": "off",
+			"jsdoc/require-param-type": "off",
+			"jsdoc/no-bad-blocks": [
+				"error",
+				{
+					ignore: ["__PURE__"],
+				},
+			],
+		},
+	},
 ];
