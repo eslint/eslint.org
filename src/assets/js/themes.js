770479408e8c21bc11e8bb62cbc70122722d5a16
akulsr0 <akulsr0@gmail.com>
2024-06-18 12:21:12 +0530

    feat: add system theme option

--- src/assets/js/themes.js
+++ src/assets/js/themes.js
@@ -3,12 +3,14 @@
     var enableToggle = function (btn) {
         btn.setAttribute("aria-pressed", "true");
     }
-    var disableToggle = function (btn) {
-        btn.setAttribute("aria-pressed", "false");
+    var disableToggle = function (btns) {
+        btns.forEach(btn => btn.setAttribute("aria-pressed", "false"));
     }
-    var setTheme = function (theme) {
+    var setTheme = function (theme, isSystemTheme) {
         document.documentElement.setAttribute('data-theme', theme);
+        document.documentElement.setAttribute('data-system-theme', isSystemTheme);
         window.localStorage.setItem("theme", theme);
+        window.localStorage.setItem("system-theme", isSystemTheme);
     }
 
     var theme = document.documentElement.getAttribute('data-theme');
@@ -17,27 +19,48 @@
         var switcher = document.getElementById('js-theme-switcher');
         switcher.removeAttribute('hidden');
         var light_theme_toggle = document.getElementById('light-theme-toggle'),
-            dark_theme_toggle = document.getElementById('dark-theme-toggle');
+            dark_theme_toggle = document.getElementById('dark-theme-toggle'),
+            system_theme_toggle = document.getElementById('system-theme-toggle');
+        var isSystemTheme = document.documentElement.getAttribute("data-system-theme") === "true";
         if (theme) {
-            if (theme === "light") {
+            if (isSystemTheme) {
+                enableToggle(system_theme_toggle);
+                disableToggle([light_theme_toggle, dark_theme_toggle]);
+                theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+                setTheme(theme, true);
+            } else if (theme === "light") {
                 enableToggle(light_theme_toggle);
-                disableToggle(dark_theme_toggle);
+                disableToggle([system_theme_toggle, dark_theme_toggle]);
             } else if (theme === "dark") {
                 enableToggle(dark_theme_toggle);
-                disableToggle(light_theme_toggle);
+                disableToggle([system_theme_toggle, light_theme_toggle]);
             }
         }
         light_theme_toggle.addEventListener("click", function () {
             enableToggle(light_theme_toggle);
             theme = this.getAttribute('data-theme');
-            setTheme(theme);
-            disableToggle(dark_theme_toggle);
+            setTheme(theme, false);
+            disableToggle([system_theme_toggle, dark_theme_toggle]);
         }, false);
         dark_theme_toggle.addEventListener("click", function () {
             enableToggle(dark_theme_toggle);
             theme = this.getAttribute('data-theme');
-            setTheme(theme);
-            disableToggle(light_theme_toggle);
+            setTheme(theme, false);
+            disableToggle([system_theme_toggle, light_theme_toggle]);
         }, false);
+        system_theme_toggle.addEventListener("click", function () {
+            enableToggle(system_theme_toggle);
+            theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+            setTheme(theme, true);
+            disableToggle([light_theme_toggle, dark_theme_toggle]);
+        }, false);
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+            const newSystemTheme = event.matches ? "dark" : "light";
+            if (isSystemTheme) {
+                enableToggle(system_theme_toggle);
+                disableToggle([light_theme_toggle, dark_theme_toggle]);
+                setTheme(newSystemTheme, true);
+            }
+        });
     }, false);
 })();
