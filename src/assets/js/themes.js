fe4771c977e09c0adf0286d6244bd66143df5485
akulsr0 <akulsr0@gmail.com>
2024-06-21 07:56:41 +0530

    refactor: remove seperate key for system theme

--- src/assets/js/themes.js
+++ src/assets/js/themes.js
@@ -6,14 +6,18 @@
     var disableToggle = function (btns) {
         btns.forEach(btn => btn.setAttribute("aria-pressed", "false"));
     }
-    var setTheme = function (theme, isSystemTheme) {
-        document.documentElement.setAttribute('data-theme', theme);
-        document.documentElement.setAttribute('data-system-theme', isSystemTheme);
-        window.localStorage.setItem("theme", theme);
-        window.localStorage.setItem("system-theme", isSystemTheme);
+    var setTheme = function (theme) {
+        if (theme === "system") {
+            theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+            document.documentElement.setAttribute('data-theme', theme);
+            window.localStorage.setItem("theme", "system");
+        } else {
+            document.documentElement.setAttribute('data-theme', theme);
+            window.localStorage.setItem("theme", theme);
+        }
     }
 
-    var theme = document.documentElement.getAttribute('data-theme');
+    var theme = window.localStorage.getItem("theme");
     
     document.addEventListener('DOMContentLoaded', function () {
         var switcher = document.getElementById('js-theme-switcher');
@@ -21,45 +25,41 @@
         var light_theme_toggle = document.getElementById('light-theme-toggle'),
             dark_theme_toggle = document.getElementById('dark-theme-toggle'),
             system_theme_toggle = document.getElementById('system-theme-toggle');
-        var isSystemTheme = document.documentElement.getAttribute("data-system-theme") === "true";
-        if (theme) {
-            if (isSystemTheme) {
-                enableToggle(system_theme_toggle);
-                disableToggle([light_theme_toggle, dark_theme_toggle]);
-                theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
-                setTheme(theme, true);
-            } else if (theme === "light") {
-                enableToggle(light_theme_toggle);
-                disableToggle([system_theme_toggle, dark_theme_toggle]);
-            } else if (theme === "dark") {
-                enableToggle(dark_theme_toggle);
-                disableToggle([system_theme_toggle, light_theme_toggle]);
-            }
+
+        if (!theme || theme === "system") {
+            enableToggle(system_theme_toggle);
+            disableToggle([light_theme_toggle, dark_theme_toggle]);
+        } else if (theme === "light") {
+            enableToggle(light_theme_toggle);
+            disableToggle([system_theme_toggle, dark_theme_toggle]);
+        } else if (theme === "dark") {
+            enableToggle(dark_theme_toggle);
+            disableToggle([system_theme_toggle, light_theme_toggle]);
         }
+
         light_theme_toggle.addEventListener("click", function () {
             enableToggle(light_theme_toggle);
             theme = this.getAttribute('data-theme');
-            setTheme(theme, false);
+            setTheme(theme);
             disableToggle([system_theme_toggle, dark_theme_toggle]);
         }, false);
         dark_theme_toggle.addEventListener("click", function () {
             enableToggle(dark_theme_toggle);
             theme = this.getAttribute('data-theme');
-            setTheme(theme, false);
+            setTheme(theme);
             disableToggle([system_theme_toggle, light_theme_toggle]);
         }, false);
         system_theme_toggle.addEventListener("click", function () {
             enableToggle(system_theme_toggle);
-            theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
-            setTheme(theme, true);
             disableToggle([light_theme_toggle, dark_theme_toggle]);
+            setTheme("system");
         }, false);
         window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-            const newSystemTheme = event.matches ? "dark" : "light";
-            if (isSystemTheme) {
+            var currentTheme = window.localStorage.getItem("theme");
+            if (currentTheme === "system" || !currentTheme) {
                 enableToggle(system_theme_toggle);
                 disableToggle([light_theme_toggle, dark_theme_toggle]);
-                setTheme(newSystemTheme, true);
+                setTheme("system");
             }
         });
     }, false);
