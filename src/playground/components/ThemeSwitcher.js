f41924d5f093d82a8c1e37ede29ca00eb582cde6
akulsr0 <akulsr0@gmail.com>
2024-06-21 13:12:10 +0530

    chore: add `aria-hidden=true` to svg icon

--- src/playground/components/ThemeSwitcher.js
+++ src/playground/components/ThemeSwitcher.js
@@ -45,7 +45,7 @@ export default function ThemeSwitcher() {
                 <button className="theme-switcher__button js-toggle-button" id="system-theme-toggle" data-theme="system" onClick={() => {
                     toggleTheme("system");
                 }} aria-pressed={theme === "system"}>
-                    <svg className="theme-switcher__icon" focusable="false" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg>
+                    <svg className="theme-switcher__icon" focusable="false" width="22" height="22" viewBox="0 0 24 24" aria-hidden="true" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg>
                     <span>System</span>
                 </button>
                 <button className="theme-switcher__button js-toggle-button" onClick={() => {
