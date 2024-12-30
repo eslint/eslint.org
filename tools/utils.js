f6193da864789010a031a24fbeeb7c1dc0cc0c80
Tanuj Kanti <tanujkanti4441@gmail.com>
2024-12-30 15:47:24 +0530

    fix linting errors in utils.js

--- tools/utils.js
+++ tools/utils.js
@@ -1,5 +1,15 @@
+/**
+ * @fileoverview Common utils for tools
+ * @author Tanuj Kanti
+ */
+
 "use strict";
 
+/**
+ * Get the avatar link of Github profile images with no additional tokens
+ * @param {Object} profile fetched data of Github users
+ * @returns {string} Github avatar link
+ */
 function getGithubAvatar(profile) {
     return `${profile.avatar_url.replace("private-", "").split("?")[0]}?v=4`
 }
