9c80c260251d03e45146e65d5cb2f08c97ec73f3
xbinaryx <xbinaryx68@gmail.com>
2025-03-05 11:23:39 +0300

    feat: add table of contents to blog post

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -89,3 +89,48 @@
 		});
 	}
 })();
+
+(function () {
+	var toc_trigger = document.getElementById("js-toc-label"),
+		toc = document.getElementById("js-toc-panel"),
+		open = false;
+
+	if (toc && matchMedia) {
+		const mq = window.matchMedia("(max-width: 1023px)");
+		mq.addEventListener("change", WidthChange);
+		WidthChange(mq);
+	}
+
+	// media query change
+	function WidthChange(mq) {
+		if (mq.matches && toc_trigger) {
+			let text = toc_trigger.innerText;
+			let headingButton = document.createElement("button");
+			headingButton.setAttribute("aria-expanded", "false");
+			headingButton.innerText = text;
+			toc_trigger.innerHTML = "";
+
+			toc_trigger.appendChild(headingButton);
+			headingButton.innerHTML += `<svg class="toc-trigger-icon" width="12" height="8" aria-hidden="true" focusable="false" viewBox="0 0 12 8"><g fill="none"><path fill="currentColor" d="M1.41.59l4.59 4.58 4.59-4.58 1.41 1.41-6 6-6-6z"/><path d="M-6-8h24v24h-24z"/></g></svg>`;
+
+			toc.setAttribute("data-open", "false");
+			toc_trigger.setAttribute("aria-expanded", "false");
+			headingButton.addEventListener("click", toggleTOC, true);
+		} else {
+			toc_trigger.innerHTML = "Table of Contents";
+			toc.setAttribute("data-open", "true");
+		}
+	}
+
+	function toggleTOC(e) {
+		if (!open) {
+			this.setAttribute("aria-expanded", "true");
+			toc.setAttribute("data-open", "true");
+			open = true;
+		} else {
+			this.setAttribute("aria-expanded", "false");
+			toc.setAttribute("data-open", "false");
+			open = false;
+		}
+	}
+})();
