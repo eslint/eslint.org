d268ad9f821aa39b696a868a2cf48f884f17868a
Amaresh  S M <amareshsm13@gmail.com>
2025-03-10 20:21:59 +0530

    feat: add code explorer in the navbar (#682)
    * feat: add code explorer in the navbar
    
    * update media query breakpoint

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,7 +5,7 @@
 		open = false;
 
 	if (matchMedia) {
-		const mq = window.matchMedia("(max-width: 680px)");
+		const mq = window.matchMedia("(max-width: 1023px)");
 		mq.addEventListener("change", WidthChange);
 		WidthChange(mq);
 	}
