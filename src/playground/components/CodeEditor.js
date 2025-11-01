5943ffbf56348c2fbe639fe999d175694ec05b1a
Pixel998 <mrhbs998@gmail.com>
2025-11-01 15:17:42 +0300

    enable JSX in editor when ecmaFeatures.jsx is true

--- src/playground/components/CodeEditor.js
+++ src/playground/components/CodeEditor.js
@@ -20,7 +20,13 @@ export default function CodeEditor({
 	const extensions = useMemo(
 		() => [
 			linter(esLint(eslintInstance, eslintOptions), { delay: 0 }),
-			javascript({ typescript: true }),
+			javascript({
+				typescript: true,
+				jsx: Boolean(
+					eslintOptions.languageOptions.parserOptions.ecmaFeatures
+						.jsx,
+				),
+			}),
 			ESLintPlaygroundTheme,
 			ESLintPlaygroundHighlightStyle,
 		],
