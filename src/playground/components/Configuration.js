9bbacbed709c8bbab1af1d6ffecf8e24efaabacb
Amaresh  S M <amareshsm13@gmail.com>
2025-12-02 00:46:00 +0530

    chore: remove unused react import from jsx files

--- src/playground/components/Configuration.js
+++ src/playground/components/Configuration.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import Select, { components } from "react-select";
 import ShareURL from "./ShareURL";
 import {
