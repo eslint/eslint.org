8e48b53703a3158ed653d4b391fa35a2922fb67a
Percy Ma <kecrily@gmail.com>
2026-08-05 22:43:36 +0800

    sitemap

--- astro.eslint.org/astro.config.mjs
+++ astro.eslint.org/astro.config.mjs
@@ -1,10 +1,30 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eslint.org",
-  integrations: [react()],
+  integrations: [
+    react(),
+    sitemap({
+      changefreq: "monthly",
+      customSitemaps: ["https://eslint.org/docs/latest/sitemap.xml"],
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          de: "de-DE",
+          en: "en-US",
+          es: "es-ES",
+          fr: "fr-FR",
+          hi: "hi-IN",
+          ja: "ja-JP",
+          "pt-br": "pt-BR",
+          "zh-hans": "zh-Hans",
+        },
+      },
+    }),
+  ],
   vite: {
     resolve: {
       dedupe: ["eslint"],
